88d3fb2c78ba9cd8bb283debc9f31695c5025313
tschuehly <tschuehly@outlook.com>
2024-03-15 21:57:22 +0100

    Initial Commit

---
+++ src/main/java/de/tschuehly/easy/spring/auth/EasySpringAuthApplication.java
@@ -0,0 +1,13 @@
+package de.tschuehly.easy.spring.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySpringAuthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EasySpringAuthApplication.class, args);
+  }
+
+}
