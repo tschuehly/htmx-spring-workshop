7eb48c4c31f9cd81775d58f4ab6c904d21012a91
tschuehly <tschuehly@outlook.com>
2024-04-22 11:04:04 +0200

    add lab-2

---
+++ lab-2/src/main/java/de/tschuehly/easy/spring/auth/user/UserService.java
@@ -0,0 +1,45 @@
+package de.tschuehly.easy.spring.auth.user;
+
+import de.tschuehly.easy.spring.auth.store.AuthRoot;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final AuthRoot authRoot = new AuthRoot();
+
+    public List<EasyUser> findAll(){
+      return authRoot.easyUserList;
+    }
+
+    public EasyUser createUser(String username, String password){
+      EasyUser newUser = new EasyUser(
+          username,
+          password
+      );
+      authRoot.easyUserList.add(newUser);
+      return newUser;
+    }
+
+
+  public EasyUser findById(UUID uuid) {
+    return authRoot.easyUserList.stream().filter(it -> Objects.equals(uuid,it.uuid)).findFirst()
+        .orElseThrow(() -> new RuntimeException("User Not Found"));
+  }
+
+  public EasyUser saveUser(UUID uuid, String username, String password) {
+    EasyUser userToUpdate = findById(uuid);
+    EasyUser newUser = new EasyUser(
+        userToUpdate.uuid,
+        username,
+        password
+    );
+    authRoot.easyUserList.set(
+        authRoot.easyUserList.indexOf(userToUpdate),
+        newUser
+    );
+    return newUser;
+  }
+}
