30a94836b4f6099fee2bf387f4c13128db327439
tschuehly <tschuehly@outlook.com>
2024-04-22 11:03:07 +0200

    add lab-7

---
+++ lab-7/src/main/java/de/tschuehly/easy/spring/auth/group/management/table/GroupTableComponent.java
@@ -0,0 +1,25 @@
+package de.tschuehly.easy.spring.auth.group.management.table;
+
+import de.tschuehly.easy.spring.auth.group.EasyGroup;
+import de.tschuehly.easy.spring.auth.group.GroupService;
+import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
+import java.util.List;
+
+@ViewComponent
+public class GroupTableComponent {
+  private final GroupService groupService;
+
+  public record GroupTableContext(List<EasyGroup> groupList) implements ViewContext{}
+
+  public final static String GROUP_TABLE_ID = "groupTable";
+  public ViewContext render(){
+    List<EasyGroup> groupList = groupService.getAll();
+    return new GroupTableContext(groupList);
+  }
+
+  public GroupTableComponent(GroupService groupService) {
+    this.groupService = groupService;
+  }
+
+}
---
+++ lab-7/src/main/java/de/tschuehly/easy/spring/auth/user/UserService.java
@@ -0,0 +1,56 @@
+package de.tschuehly.easy.spring.auth.user;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+  public final static List<EasyUser> easyUserList = new ArrayList<>();
+
+  public List<EasyUser> findAll() {
+    return easyUserList;
+  }
+
+  public EasyUser createUser(String username, String password) {
+    EasyUser newUser = new EasyUser(
+        username,
+        password
+    );
+    easyUserList.add(newUser);
+    return newUser;
+  }
+
+  public List<EasyUser> searchUser(String searchString) {
+    return easyUserList.stream().filter(
+        it -> it.uuid.toString().contains(searchString)
+              || it.username.contains(searchString)
+              || it.password.contains(
+            searchString)
+    ).toList();
+  }
+
+
+  public EasyUser findById(UUID uuid) {
+    return easyUserList.stream().filter(it -> Objects.equals(uuid, it.uuid)).findFirst()
+        .orElseThrow(() -> new RuntimeException("User Not Found"));
+  }
+
+  public EasyUser saveUser(UUID uuid, String username, String password) {
+    EasyUser userToUpdate = findById(uuid);
+    EasyUser newUser = new EasyUser(
+        userToUpdate.uuid,
+        username,
+        password
+    );
+    easyUserList.set(
+        easyUserList.indexOf(userToUpdate),
+        newUser
+    );
+    return newUser;
+  }
+}
