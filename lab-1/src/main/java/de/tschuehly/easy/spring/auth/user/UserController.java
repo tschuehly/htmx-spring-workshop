6a33085cf0087c74bfb38e8b6d9b98fc9c113faf
tschuehly <tschuehly@outlook.com>
2024-05-26 17:36:41 +0200

    lab-1-checkpoint-1

--- lab-1/src/main/java/de/tschuehly/easy/spring/auth/user/UserController.java
+++ lab-1/src/main/java/de/tschuehly/easy/spring/auth/user/UserController.java
@@ -1,9 +1,13 @@
 package de.tschuehly.easy.spring.auth.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+  public static final String MODAL_CONTAINER_ID = "modalContainer"; // (1)
+  public static final String USER_TABLE_BODY_ID = "userTableBody"; // (1)
 
   private final UserService userService;
 
@@ -11,4 +15,9 @@ public class UserController {
     this.userService = userService;
   }
 
+  @GetMapping("/") // (2)
+  public String index(Model model) {
+    model.addAttribute("easyUserList", userService.findAll()); // (3)
+    return "UserManagement";
+  }
 }
