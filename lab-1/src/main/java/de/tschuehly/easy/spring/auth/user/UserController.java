3b324deb5d6f58cea55312221f6a855e2db9db2e
tschuehly <tschuehly@outlook.com>
2024-05-26 17:39:25 +0200

    lab-1-checkpoint-2

--- lab-1/src/main/java/de/tschuehly/easy/spring/auth/user/UserController.java
+++ lab-1/src/main/java/de/tschuehly/easy/spring/auth/user/UserController.java
@@ -3,6 +3,9 @@ package de.tschuehly.easy.spring.auth.user;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
 
 @Controller
 public class UserController {
@@ -20,4 +23,17 @@ public class UserController {
     model.addAttribute("easyUserList", userService.findAll()); // (3)
     return "UserManagement";
   }
+
+  public record UserForm(String uuid, String username, String password) {} // (1)
+
+  public static final String GET_EDIT_USER_MODAL = "/save-user/modal/{uuid}"; // (2)
+
+  @GetMapping(GET_EDIT_USER_MODAL)
+  public String editUserModal(Model model, @PathVariable UUID uuid) {
+    var user = userService.findById(uuid);
+    model.addAttribute("userForm",
+            new UserForm(user.uuid.toString(), user.username, user.password)); // (3)
+    return "EditUserForm";
+  }
+
 }
