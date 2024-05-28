44e66dcc033abf54c4a481b9ff209ef46db75b1a
tschuehly <tschuehly@outlook.com>
2024-05-28 15:14:57 +0200

    lab-4-checkpoint-3

--- lab-4/src/main/java/de/tschuehly/easy/spring/auth/user/management/UserManagementComponent.java
+++ lab-4/src/main/java/de/tschuehly/easy/spring/auth/user/management/UserManagementComponent.java
@@ -2,9 +2,11 @@ package de.tschuehly.easy.spring.auth.user.management;
 
 import de.tschuehly.easy.spring.auth.user.UserController;
 import de.tschuehly.easy.spring.auth.web.Page;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(1)
 public class UserManagementComponent implements Page {
 
     @Override
