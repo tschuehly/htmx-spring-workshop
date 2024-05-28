5e82a4eb33c6eeac68cdf3f515814d55f62af7df
tschuehly <tschuehly@outlook.com>
2024-05-28 15:10:36 +0200

    lab-4-checkpoint-2

--- lab-4/src/main/java/de/tschuehly/easy/spring/auth/user/management/table/row/UserRowComponent.java
+++ lab-4/src/main/java/de/tschuehly/easy/spring/auth/user/management/table/row/UserRowComponent.java
@@ -1,6 +1,5 @@
 package de.tschuehly.easy.spring.auth.user.management.table.row;
 
-import static de.tschuehly.easy.spring.auth.user.management.UserManagementComponent.CLOSE_MODAL_EVENT;
 
 import de.tschuehly.easy.spring.auth.htmx.HtmxUtil;
 import de.tschuehly.easy.spring.auth.user.EasyUser;
@@ -9,35 +8,38 @@ import de.tschuehly.easy.spring.auth.user.management.table.UserTableComponent;
 import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
 import de.tschuehly.spring.viewcomponent.jte.ViewContext;
 import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxSwapType;
+
 import java.util.UUID;
+
+import static de.tschuehly.easy.spring.auth.web.layout.LayoutComponent.CLOSE_MODAL_EVENT;
 
 @ViewComponent
 public class UserRowComponent {
 
-  public record UserRowContext(EasyUser easyUser) implements ViewContext {
+    public record UserRowContext(EasyUser easyUser) implements ViewContext {
 
-    public static String htmlUserId(UUID uuid) {
-      return "user-" + uuid;
+        public static String htmlUserId(UUID uuid) {
+            return "user-" + uuid;
+        }
     }
-  }
 
-  public ViewContext render(EasyUser easyUser) {
-    return new UserRowContext(easyUser);
-  }
+    public ViewContext render(EasyUser easyUser) {
+        return new UserRowContext(easyUser);
+    }
 
-  public ViewContext rerender(EasyUser easyUser) {
-    String target = HtmxUtil.target(UserRowContext.htmlUserId(easyUser.uuid));
-    HtmxUtil.retarget(target);
-    HtmxUtil.reswap(HxSwapType.OUTER_HTML);
-    HtmxUtil.trigger(CLOSE_MODAL_EVENT);
-    return new UserRowContext(easyUser);
-  }
+    public ViewContext rerender(EasyUser easyUser) {
+        String target = HtmxUtil.target(UserRowContext.htmlUserId(easyUser.uuid));
+        HtmxUtil.retarget(target);
+        HtmxUtil.reswap(HxSwapType.OUTER_HTML);
+        HtmxUtil.trigger(CLOSE_MODAL_EVENT);
+        return new UserRowContext(easyUser);
+    }
 
-  public ViewContext renderNewRow(EasyUser user) {
-    String target = HtmxUtil.target(UserTableComponent.USER_TABLE_BODY_ID);
-    HtmxUtil.retarget(target);
-    HtmxUtil.reswap(HxSwapType.AFTER_BEGIN);
-    HtmxUtil.trigger(UserManagementComponent.CLOSE_MODAL_EVENT);
-    return new UserRowContext(user);
-  }
+    public ViewContext renderNewRow(EasyUser user) {
+        String target = HtmxUtil.target(UserTableComponent.USER_TABLE_BODY_ID);
+        HtmxUtil.retarget(target);
+        HtmxUtil.reswap(HxSwapType.AFTER_BEGIN);
+        HtmxUtil.trigger(CLOSE_MODAL_EVENT);
+        return new UserRowContext(user);
+    }
 }
